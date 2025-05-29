acfd19bcdbf98bb7f4ce991d3ec3300f75bfc0d6
Zunair-Ali <zunairliaqat375@gmail.com>
2025-05-29 20:09:54 +0500

    Save

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
 
 const Header = () => {
+
+  const {removeBg} = useContext(AppContext)
+
   return (
     <div className="flex items-center justify-between max-sm:flex-col-reverse gap-y-10 px-2 mt-10 lg:px-20 sm:mx-20">
       {/* Left Side */}
@@ -19,7 +23,7 @@ const Header = () => {
           ever.
         </p>
         <div className=" mx-auto">
-          <input type="file" id="upload1" hidden />
+          <input onChange={e=>removeBg(e.target.files[0])} accept="image/" type="file" id="upload1" hidden />
           <label
             className="inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r  from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700"
             htmlFor="upload1"
---
+++ server/controllers/imageController.js
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import fs from 'fs'
+import formData from 'form-data'
+import userModel from '../models/userModel.js'
+
+// Controller function to remove bg from image
+
+const removeBgImage = async (req,res) => {
+    try {
+        const {clerkId} = req.body
+        const user = await userModel.findOne({clerkId})
+        if(!user){
+            return res.json({success:false,message:'User Not Found'})
+        }
+        if(user.creditBalance=== 0){
+            return res.json({success:false,message:"not credit balance",creditBalance:user.creditBalance})
+        }
+        const imagePath = req.file.path;
+        // Reading the image file
+        const imageFile = fs.createReadStream(imagePath)
+
+        const formdata = new FormData()
+
+        formdata.append('image_file',imageFile)
+
+        const {data} = await axios.post('https://clipdrop-api.co/remove-background/v1',formdata,{
+            headers :{
+                'x-api-key': process.env.CLIPDROP_API
+            },
+            responseType : 'arraybuffer'
+        })
+        const base64Image = Buffer.from(data,'binary').toString('base64')
+
+        const resultImage = `data:${req.file.minetype};base64,${base64Image}`
+
+        await userModel.findByIdAndUpdate(user._id,{creditBalance:user.creditBalance - 1})
+
+        res.json({success:true,resultImage,creditBalance:userCredits.creditBalance-1,message:"Background Removed"})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {removeBgImage}
